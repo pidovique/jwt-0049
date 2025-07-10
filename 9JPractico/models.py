c61637d28b23d807638a1daf803f6cf2aba4cf76
daniel-f82 <169076225+daniel-f82@users.noreply.github.com>
2025-07-10 17:00:28 -0600

    Create models.py

---
+++ 9JPractico/models.py
@@ -0,0 +1,43 @@
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime
+
+
+db = SQLAlchemy()
+
+
+class User(db.Model):
+    __tablename__ = 'usuarios'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
+    role = db.Column(db.String(20), default='user', nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    is_active = db.Column(db.Boolean, default=True)
+
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+    
+
+    def is_admin(self):
+        return self.role == 'admin'
+    
+    def to_dict(self):
+        return {
+
+            'id': self.id,
+            'username' : self.username,
+            'email' : self.email,
+            'role' : self.role,
+            'create_at' : self.created_at.isoformat(),
+            'is_active' : self.is_active
+
+
+        }
